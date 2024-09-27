76b03f3d7c7a350b1f377cf7a53a9b7ffe3cea7b
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-09-27 11:42:59 +0530

    add Not Found Page , fix menu , account Dropdown

--- frontend/src/views/NotFound.js
+++ frontend/src/views/NotFound.js
@@ -1,7 +1,14 @@
 
-import DashboardLayout from "./DashboardLayout";
+import { Button, Result } from "antd";
 
 const NotFound = () => {
-    return <p>Not Found</p>
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+            extra={<Button type="primary">Back Home</Button>}
+        />
+    )
 }
 export default NotFound;
