edd28e819dd13939d543ed0a4420f746e400c849
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-01 10:33:17 +0530

    add a cors of ui in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,9 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://accufirm.vercel.app',
+}));
 app.use(express.json());
 
 // MongoDB connection
@@ -19,10 +21,11 @@ app.use(express.json());
 // Routes
 app.get("/", (req, res) => {
   res.send("AccuFirm API is running!");
+  res.send("an Accounting App")
 });
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ->  ${PORT}`);
 });
