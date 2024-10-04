42b4d22e420ae6fe2ed6678cf48a61b9d756f4d7
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-04 12:27:42 +0530

    update code

--- frontend/src/router/PrivateRoute.js
+++ frontend/src/router/PrivateRoute.js
@@ -3,8 +3,8 @@ import { Navigate } from "react-router-dom";
 import * as authService from "../auth";
 
 const PrivateRoute = ({ component: Component }) => {
-    //return authService.token.get() ? <Component /> : <Navigate to="/login" />;
-    return  <Component />
+    return authService.token.get() ? <Component /> : <Navigate to="/login" />;
+    //return  <Component />
 };
 
 export default PrivateRoute;
--- frontend/src/router/PublicRoute.js
+++ frontend/src/router/PublicRoute.js
@@ -3,8 +3,8 @@ import { Navigate } from "react-router-dom";
 import * as authService from "../auth";
 
 const PublicRoute = ({ component: Component }) => {
-    //return authService.token.get() ? <Navigate to="/" /> : <Component />;
-     return <Component />
+    return authService.token.get() ? <Navigate to="/" /> : <Component />;
+    //return <Component />
 };
 
 export default PublicRoute;
