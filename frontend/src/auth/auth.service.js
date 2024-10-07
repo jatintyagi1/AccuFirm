aed6523e29f6a94e88832436b962cb38a8fdad0a
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-07 10:46:06 +0530

    add cookies

--- frontend/src/auth/auth.service.js
+++ frontend/src/auth/auth.service.js
@@ -1,40 +1,39 @@
 import { API_BASE_URL, ACCESS_TOKEN_NAME } from "../config/serverApiConfig";
-import Cookies from "js-cookie";
+
+
 import axios from "axios";
 import errorHandler from "../request/errorhandler";
 import successHandler from "../request/successHandler";
 import storePersist from "../redux/storePersist";
 
-// Token management
-export const token = {
-  get: () => Cookies.get(ACCESS_TOKEN_NAME),
+import { getCookie, setCookie, deleteCookie } from "./cookie";
 
-  set: (newToken) => {
-    if (newToken) {
-      Cookies.set(ACCESS_TOKEN_NAME, newToken);
-    }
-  },
-
-  remove: () => Cookies.remove(ACCESS_TOKEN_NAME),
-};
-
-// Login function
 export const login = async (loginUserData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/login`, loginUserData);
-    
-    if (response?.data?.result?.token) {
-      token.set(response.data.result.token);
-    }
-
+    const response = await axios.post(
+      API_BASE_URL + `login?timestamp=${new Date().getTime()}`,
+      loginUserData
+    );
+    token.set(response.data.result.token);
     return successHandler(response);
   } catch (error) {
     return errorHandler(error);
   }
 };
 
-// Logout function
 export const logout = () => {
   token.remove();
   storePersist.clear();
+};
+
+export const token = {
+  get: () => {
+    return getCookie(ACCESS_TOKEN_NAME);
+  },
+  set: (token) => {
+    return setCookie(ACCESS_TOKEN_NAME, token);
+  },
+  remove: () => {
+    return deleteCookie(ACCESS_TOKEN_NAME);
+  },
 };
