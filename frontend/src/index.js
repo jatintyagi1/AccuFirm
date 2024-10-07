9a66e8911c913e110174792b2112401299526507
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-07 10:39:11 +0530

    main app code refactor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./app/index";
+import './app/style.less';
 import reportWebVitals from "./reportWebVitals";
 import *  as serviceWorker from './ServiceWorker';
 
