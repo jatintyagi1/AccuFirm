644fd97f08511828a541234105830512f9c14111
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-10-16 11:53:52 +0530

    add alies path in vite.config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path'; // Import resolve to set up the alias
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // Set `@` to point to the `src` directory
+    },
+  },
+});
