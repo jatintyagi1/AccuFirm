ed607e62c74daee6dc473fb52f78b9503ff53776
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-11-07 10:23:21 +0530

    add a login form

--- frontend/src/forms/LoginForm.jsx
+++ frontend/src/forms/LoginForm.jsx
@@ -1,59 +1,56 @@
+import React from 'react';
 import { Form, Input, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
+import useLanguage from '@/locale/useLanguage';
 
 export default function LoginForm() {
-    return (
-        <>
-            <Form.Item
-                label='email'
-                name="email"
-                rules={[
-                    {
-                        required: true,
+  const translate = useLanguage();
+  return (
+    <div>
+      <Form.Item
+        label={translate('email')}
+        name="email"
+        rules={[
+          {
+            required: true,
+          },
+          {
+            type: 'email',
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder={'admin@demo.com'}
+          type="email"
+          size="large"
+        />
+      </Form.Item>
+      <Form.Item
+        label={translate('password')}
+        name="password"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input.Password
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          placeholder={'admin123'}
+          size="large"
+        />
+      </Form.Item>
 
-                    },
-                    {
-                        type: 'email',
-                    },
-                ]}
-
-            >
-                <Input
-                    prefix={<UserOutlined className="site-form-item-icon" />}
-                    placeholder='email'
-                    type="email"
-                    size="large"
-                />
-            </Form.Item>
-            <Form.Item
-                label='password'
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                    },
-                ]}
-            >
-                <Input.Password
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    placeholder='password'
-                    size="large"
-                />
-            </Form.Item>
-
-            <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-                <a className="login-form-forgot" href="/forgetpassword"
-                style={{ marginLeft: "200px"}}
-                >
-                    Forgot password
-                </a>
-
-            </Form.Item>
-
-        </>
-    )
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>{translate('Remember me')}</Checkbox>
+        </Form.Item>
+        <a className="login-form-forgot" href="/forgetpassword" style={{ marginLeft: '0px' }}>
+          {translate('Forgot password')}
+        </a>
+      </Form.Item>
+    </div>
+  );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,49 +1,68 @@
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
 import { Form, Button } from 'antd';
 
-import AuthModule from '../modules/AuthModule'
+import { login } from '../redux/auth/actions';
+import { selectAuth } from '../redux/auth/selectors';
 import LoginForm from '../forms/LoginForm';
+import Loading from '../components/Loading';
+import AuthModule from '../modules/AuthModule';
 
 const LoginPage = () => {
+  const { isLoading, isSuccess } = useSelector(selectAuth);
+  const navigate = useNavigate();
 
-    const onFinish = (values) => {
-        console.log(values)
-    }
+  const dispatch = useDispatch();
+  const onFinish = (values) => {
+    dispatch(login({ loginData: values }));
+  };
 
-    const FormContainer = () => {
-        return (
-            <Form
-                layout="vertical"
-                name="normal_login"
-                className="login-form"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
+  useEffect(() => {
+    if (isSuccess) navigate('/');
+  }, [isSuccess]);
+
+  const FormContainer = () => {
+    return (
+      <Loading isLoading={isLoading}>
+        <Form
+          layout="vertical"
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <LoginForm />
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              loading={isLoading}
+              size="large"
             >
-                <LoginForm />
-                <Form.Item>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        className="login-form-button"
-                        size="large"
-                    >
-                        Log in
-                    </Button>
+              Log in
+            </Button>
+            <Button
+              href='/register'
+              type='primary'
+              className='login-form-button'
+              size='large'
+            >
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </Loading>
+    );
+  };
 
-                    <Button
-                        type='primary'
-                        htmlType='button'
-                        className='login-form-button'
-                        size='large'
-                    >
-                        Register
-                    </Button>
-                </Form.Item>
-            </Form>
-        )
-    };
+  return <AuthModule authContent={<FormContainer />} AUTH_TITLE="Sign in" />;
+};
 
-    return <AuthModule authContent={<FormContainer />} AUTH_TITLE="Sign in" />
-}
 export default LoginPage;
